231b6d83a59426a565875e155d859e71abd8ee5e
Wolfero <ferorock44@gmail.com>
2023-01-21 12:33:23 +0000

    ✨add exercise

---
+++ 02-lenguages/02-concat/main.ts
@@ -0,0 +1,28 @@
+const concat = (a: number[], b: number[]) => {
+    const newA = [...a];
+    const newB = [...b];
+    return [...newA, ...newB];
+};
+
+const arrayA = [1, 2];
+const arrayB = [3, 4];
+const result = concat(arrayA, arrayB);
+console.log(result);
+
+//OPTIONAL CONCAT
+const optionalConcat = (...arg: number[][]) => {
+    const [a, b, ...rest] = [...arg];
+    return [...a, ...b].concat(...rest);
+};
+
+const optionalArrayA = [1, 2];
+const optionalArrayB = [3, 4];
+const optionalArrayC = [5, 6];
+const optionalArrayD = [7, 8];
+const optionalResult = optionalConcat(
+    optionalArrayA,
+    optionalArrayB,
+    optionalArrayC,
+    optionalArrayD
+);
+console.log(optionalResult);
