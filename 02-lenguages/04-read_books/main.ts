4cffa95d1a708428b9ac98891b1b246751ef6af9
Wolfero <ferorock44@gmail.com>
2023-01-22 10:05:42 +0000

    ✨add exercise

---
+++ 02-lenguages/04-read_books/main.ts
@@ -0,0 +1,21 @@
+interface Book {
+    title: string;
+    isRead: boolean;
+}
+
+function isBookRead(books: Book[], titleToSearch: string) {
+    const isRead = true;
+    const book = books.find((book) => book.title === titleToSearch);
+    if (!book) return false;
+
+    return book.isRead ? isRead : !isRead;
+}
+
+const books: Book[] = [
+    { title: 'Harry Potter y la piedra filosofal', isRead: true },
+    { title: 'Canción de hielo y fuego', isRead: false },
+    { title: 'Devastación', isRead: true },
+];
+console.log(isBookRead(books, 'Devastación')); // expected -> true
+console.log(isBookRead(books, 'Canción de hielo y fuego')); // expected -> false because is not read
+console.log(isBookRead(books, 'Los Pilares de la Tierra')); // expected -> false because the book isn't exist
