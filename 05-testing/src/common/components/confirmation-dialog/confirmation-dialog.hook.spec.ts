8387c543cefc53e7e51be71e2896a100f7928d60
Wolfero <ferorock44@gmail.com>
2023-07-22 14:51:59 +0100

    ✅ Add test handle opening the dialog

---
+++ 05-testing/src/common/components/confirmation-dialog/confirmation-dialog.hook.spec.ts
@@ -0,0 +1,19 @@
+import { renderHook, act } from '@testing-library/react';
+import { Lookup } from 'common/models';
+import { useConfirmationDialog } from './confirmation-dialog.hook';
+
+describe('useConfirmationDialog hook should', () => {
+  const mockedLookup: Lookup = { id: '1', name: 'Test Item' };
+
+  it('handle opening the dialog', () => {
+    const { result } = renderHook(() => useConfirmationDialog());
+
+    expect(result.current.isOpen).toBe(false);
+    act(() => {
+      result.current.onOpenDialog(mockedLookup);
+    });
+
+    expect(result.current.isOpen).toBe(true);
+    expect(result.current.itemToDelete).toEqual(mockedLookup);
+  });
+});
