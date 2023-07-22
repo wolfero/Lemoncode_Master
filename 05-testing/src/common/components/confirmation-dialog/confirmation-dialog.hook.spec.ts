924f2c64b512c160a96b864e549005411995d48a
Wolfero <ferorock44@gmail.com>
2023-07-22 14:53:30 +0100

    ✅ Add test handle closing the dialog

--- 05-testing/src/common/components/confirmation-dialog/confirmation-dialog.hook.spec.ts
+++ 05-testing/src/common/components/confirmation-dialog/confirmation-dialog.hook.spec.ts
@@ -16,4 +16,21 @@ describe('useConfirmationDialog hook should', () => {
     expect(result.current.isOpen).toBe(true);
     expect(result.current.itemToDelete).toEqual(mockedLookup);
   });
+  
+  it('handle closing the dialog', () => {
+    const { result } = renderHook(() => useConfirmationDialog());
+
+    act(() => {
+      result.current.onOpenDialog(mockedLookup);
+    });
+    expect(result.current.isOpen).toBe(true);
+
+    act(() => {
+      result.current.onClose();
+    });
+
+    expect(result.current.isOpen).toBe(false);
+  });
+
+
 });
